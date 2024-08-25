92b56b45c92d7b4954a8fec95607ae388d0c37ce
mrgawde <mehulgawdenxt@gmail.com>
2024-08-25 11:49:21 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Josefin_Sans } from "next/font/google"
 import { motion } from "framer-motion";
 
 import { cn } from "@/lib/utils"
-import { AuroraBackground } from "@/components/ui/aurora-background";
 
 const font = Josefin_Sans({ weight: ['400'], style: "normal", subsets: ["latin"] });
 
